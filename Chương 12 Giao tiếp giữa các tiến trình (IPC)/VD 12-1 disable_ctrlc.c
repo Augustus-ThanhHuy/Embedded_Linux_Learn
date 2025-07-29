652dbbf832c4f7796fa152a38304963c5dc41fa1
sano <huy.levi.18@gmail.com>
2025-07-29 21:13:30 +0700

    feat: add signal basic

---
+++ Chương 12 Giao tiếp giữa các tiến trình (IPC)/VD 12-1 disable_ctrlc.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
+void catch_int(int sig_num)
+{
+    //khôi phục lại bộ xử lý tín hiệu để có thể bẫy lại tín hiệu trong lần kế tiếp
+    signal(SIGINT, catch_int);
+    printf("Don't press Ctrl-C to stop me\n");
+}
+
+int main(int argc, char* argv[])
+{
+    int count = 0;
+
+    //Thiết lập hàm xử lý cho tín hiệu INT (Ctrl-C)
+    signal(SIGINT, catch_int);
+
+    while(1){
+        printf("Process is calculating ...%d\n", count++);
+        sleep(1);
+    }
+}
